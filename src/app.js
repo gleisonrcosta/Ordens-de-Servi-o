95225f6a73fe92c46395250b12dd72260c353ede
Gleison <gleisonrcosta@gmail.com>
2026-08-05 10:26:34 -0300

    atualiza pagina

--- src/app.js
+++ src/app.js
@@ -165,12 +165,22 @@ app.get("/events", requireAuth, async (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
+  res.setHeader("X-Accel-Buffering", "no");
   res.flushHeaders?.();
   res.write("event: ready\ndata: connected\n\n");
 
   sseClients.add(res);
+  const heartbeat = setInterval(() => {
+    try {
+      res.write(": ping\n\n");
+    } catch (error) {
+      clearInterval(heartbeat);
+      sseClients.delete(res);
+    }
+  }, 25000);
 
   req.on("close", () => {
+    clearInterval(heartbeat);
     sseClients.delete(res);
   });
 });
